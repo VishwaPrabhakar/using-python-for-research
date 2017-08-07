d7ee993074e1b65c76760b430e83a223235c8e9b
VishwaPrabhakar Singh <vishwaprabhakarsingh@gmail.com>
2017-08-07 23:04:24 +0530

    Update ex3.py
    only need to create function and call it no need to print anything.

--- Week2_PythonLibsanConceptsUsedInResearch/ex/ex3.py
+++ Week2_PythonLibsanConceptsUsedInResearch/ex/ex3.py
@@ -6,34 +6,35 @@
 # (board) to see what it returns!
 
 import numpy as np
-from Week2_PythonLibsanConceptsUsedInResearch.ex.ex2 import create_board, place
+#from Week2_PythonLibsanConceptsUsedInResearch.ex.ex2 import create_board, place
 # from .ex2 import create_board, place # This will cause relative import error?
-
-# Ex3
+# write your code here!
+import numpy as np
 def possibilities(brd):
-    """
-    :param brd:
-    :return:
-    """
     return list(zip(*np.where(brd == 0)))
     # return np.where(brd) == 0
 
-if __name__ == '__main__':
-    board = create_board()
+##if __name__ == '__main__':
+  #  board = create_board()
 
     # Fill in positions where still zero
-    print(place(board, 1, (0, 0)))
+  #  print(place(board, 1, (0, 0)))
     # print(place(board, 1, (0, 1)))
     # print(place(board, 1, (0, 2)))
     # print(place(board, 1, (1, 0)))
-    print(place(board, 1, (1, 1)))
+   # print(place(board, 1, (1, 1)))
     # print(place(board, 1, (1, 2)))
     # print(place(board, 1, (2, 0)))
     # print(place(board, 1, (2, 1)))
-    print(place(board, 1, (2, 2)))
+   # print(place(board, 1, (2, 2)))
 
     # Get open positions still left
-    print(possibilities(board))
+    #print(possibilities(board))
+
+possibilities(board)
+
+
+
 
 # [[1 0 0]
 #  [0 0 0]
